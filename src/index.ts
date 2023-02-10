511306f8823a4775749482ce09779425604f04a3
Jefferson Aguilar <jeffersonaguilar@tyba.com.co>
2023-02-09 21:20:46 -0500

    1.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,40 @@
 export const Normalize = (data: any) => {
-  const isObject = (dataObj: any) =>
-      Object.prototype.toString.call(dataObj) === '[object Object]'
-  const isArray = (dataObj: any) =>
-      Object.prototype.toString.call(dataObj) === '[object Array]'
+  const isObject = (dataObj: any) => Object.prototype.toString.call(dataObj) === '[object Object]';
+  const isArray = (dataObj: any) => Object.prototype.toString.call(dataObj) === '[object Array]';
 
   const flatten = (dataObj: any) => {
     if (!dataObj.attributes) {
-      return dataObj
+      return dataObj;
     }
 
     return {
       id: dataObj.id,
-      ...dataObj.attributes
-    }
-  }
+      ...dataObj.attributes,
+    };
+  };
 
   if (isArray(data)) {
-    return data.map((item: any) => Normalize(item))
+    return data.map((item: any) => Normalize(item));
   }
 
   if (isObject(data)) {
     if (isArray(data.data)) {
-      data = [...data.data]
+      data = [...data.data];
     } else if (isObject(data.data)) {
-      data = flatten({ ...data.data })
+      data = flatten({ ...data.data });
     } else if (data.data === null) {
-      data = null
+      data = null;
     } else {
-      data = flatten(data)
+      data = flatten(data);
     }
 
     // tslint:disable-next-line:forin
     for (const key in data) {
-      data[key] = Normalize(data[key])
+      data[key] = Normalize(data[key]);
     }
 
-    return data
+    return data;
   }
 
-  return data
-}
+  return data;
+};
