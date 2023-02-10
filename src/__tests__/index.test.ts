d7589c4499eb82942acceb4f8d89ccfdb0c27149
Jefferson Aguilar <jeffersonaguilar@tyba.com.co>
2023-02-09 20:32:19 -0500

    Add test suites and solve coverage

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,19 @@
+import { Normalize } from '../index';
+import { data, result } from './data';
+
 describe('Init', () => {
-    test('should return true', () => {
-        expect(true).toBe(true);
-    });
+  test('should return true', () => {
+    const resultData = Normalize(data);
+    expect(resultData).toEqual(result);
+  });
+
+  test('should return true', () => {
+    const resultData = Normalize({ data: null });
+    expect(true).toBe(true);
+    expect(resultData).toBeNull();
+  });
+  test('should return true', () => {
+    const resultData = Normalize({ data: [] });
+    expect(resultData).toEqual([]);
+  });
 });
